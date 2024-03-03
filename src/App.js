b4dac15de555ce4ee5962034323b567681a7ef2e
adibackend <adityagohar3@gmail.com>
2024-03-03 23:50:19 +0200

    Applied Spread Operator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const links =  [
+const links = [
   {
     "url": "https://m.media-amazon.com/images/I/616ayioUd6L._SL1500_.jpg",
     "name": "Same As Ever",
     'authorName': 'Morgan Housel',
+    id: 1,
   },
   {
-    "url": "https://m.media-amazon.com/images/I/61E0unKRIZL._SL1500_.jpg",
+    "url": "https://m.media-amazon.com/images/I/71NM33ZCE1L._SL1000_.jpg",
     "name": "11 Rules For Life: Secrets to Level Up",
     "authorName": "Chetan Bhagat",
+    id: 2,
   },
   {
 
@@ -17,6 +19,7 @@ const links =  [
     "url": "https://m.media-amazon.com/images/I/71M+MkYZA9L._SL1500_.jpg",
     "name": "Free Will",
     "authorName": "Tim harris",
+    id: 4,
   }
 ]
 
@@ -26,39 +29,38 @@ function App() {
   const BookList = () => {
     return (
       <>
-       
-            <Book />
+        <div className='flex justify-center items-center h-screen bg-[#f1f1f1] space-x-4'>
 
+        {links.map((link) => {
+          return (
+            <>
+              <Book {...link} key={link.id} />
+            </>
+          )
+        })}
 
+        </div>
       </>
     )
   }
 
-  const Book = () => {
+  const Book = (props) => {
+    const { name, authorName, url } = props
     return (
       <>
-      <div className='flex justify-center items-center h-screen gap-5 bg-[#f1f1f1] '>
+  <div className='bg-blue-500 rounded-xl  pb-4'>
+    <div>
+        <img src={url} alt='Book image' className='h-[300px] w-[200px]  p-4'></img>
+    </div>
 
-      {links.map((n)=>{
-        return(
-          <>
-       <article >
-              <div className='bg-blue-500 pb-4 rounded-xl max-w-xs'>
+        <section className='text-center  px-4 space-y-2 '>
+          <h1 className='max-w-40 font-semibold text-xl'>{name}</h1>
+          <p>{authorName}</p>
 
-        <section>
-
-          <img src={n.url} alt='asdf' className='h-[300px] w-[200px] p-4 '></img>
         </section>
-        <section className='text-center  px-4 '>
-                  <h1 className='max-w-44'>{n.name}</h1>
-<p>{n.authorName}</p>
-        </section>
-      </div>
-       </article>
-          </>
-        )
-      })}
-      </div>
+  </div>
+
+
       </>
     )
   }
