91f1e56f289d1d7968aea88bcd83bd209ed6e552
adibackend <adityagohar3@gmail.com>
2024-03-03 02:31:36 +0200

    made card componets, still a lot to do!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 
 function App() {
+  const BookList = () => {
+    return (
+      <>
+
+        <div className="my-auto">
+
+          <section className="grid  grid-cols-3 mx-auto  max-w-3xl my-auto gap-x-16 ">
+            <Book />
+            <Book />
+            <Book />
+          </section>
+        </div>
+
+
+      </>
+    )
+  }
+  const Book = () => {
+    return (
+      <>
+        <article className="bg-[#FFFFFF] rounded-xl font-semibold">
+          <Image />
+          <div className="space-y-2">
+
+            <Title />
+            <Author />
+          </div>
+        </article>
+      </>
+    )
+  }
+  const Image = () => {
+    return (
+      <>
+        <div className="p-4">
+
+          <img src="https://m.media-amazon.com/images/I/616ayioUd6L._SL1500_.jpg
+" className="object-fill " alt="image hai" />
+        </div>
+
+      </>
+    )
+  }
+  const Title = () => {
+    return (
+      <>
+        <h2 className="text-center text-xl">Title</h2>
+      </>
+    )
+  }
+  const Author = () => {
+    return (
+      <>
+        <h2 className="text-center  pb-4">Author</h2>
+      </>
+    )
+  }
   return (
-   <>
-   <h1 className="under">adsf</h1>
-   </>
+    <>
+      <div className="bg-[#f1f5f8] h-screen flex items-center justify-center">
+        <BookList />
+      </div>
+    </>
   );
 }
 
